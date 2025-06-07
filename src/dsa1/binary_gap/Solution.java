a746c165a7859c0735af6c7989438a578b750671
Tat Uyen Tam <tattam192@gmail.com>
2025-06-07 17:57:07 +1000

    complete change money exercise

--- src/dsa1/binary_gap/Solution.java
+++ src/dsa1/binary_gap/Solution.java
@@ -1,17 +1,19 @@
 package dsa1.binary_gap;
 
 public class Solution {
+	// Time complexity: O(log(N))
 	public static int solution(int N) {
 		int maxGap = 0;
 		int prev = -1;
 		int i = 0;
-		// Time complexity of below group of code: O(log2(N))
 		do {
 			int r = N%2;
 			
 			if (r==1) {
-				int gap = i - prev - 1;
-				maxGap = (prev>=0 && gap>maxGap) ? gap : maxGap;
+				if (prev!=-1) {
+					int gap = i - prev - 1;
+					maxGap = (prev>=0 && gap>maxGap) ? gap : maxGap;
+				}
 				prev = i;
 			}
 			
