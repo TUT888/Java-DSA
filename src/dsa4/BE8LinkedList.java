724827c15163f92b4ac790590feb521ac5bd3186
Tat Uyen Tam <tattam192@gmail.com>
2025-06-11 16:04:31 +1000

    linkedlist exercise

---
+++ src/dsa4/BE8LinkedList.java
@@ -0,0 +1,62 @@
+package dsa4;
+
+public class BE8LinkedList {
+	LinkedListItem head;
+	
+	// Insert at any index
+	// Time: O(index)
+	// Space: O(1) 
+	public boolean insert(int index, int value) {
+		if (head == null) {
+			head = new LinkedListItem(value);
+			return true;
+		}
+		
+		if (index == 0) {
+			head = new LinkedListItem(value, head);
+			return true;
+		}
+		
+		LinkedListItem item = head;
+		int i = 0;
+		while (i < index-1) {
+			i++;
+			item = item.next;
+			if (item == null) {
+				// index is out of linkedlist's range
+				return false;
+			}
+		}
+		
+		item.next = new LinkedListItem(value, item.next);
+		return true;
+	}
+	
+	// Delete any index
+	// Time: O(index)
+	// Space: O(1) 
+	public boolean delete(int index) {
+		if (head == null) {
+			return false;
+		}
+
+		if (index == 0) {
+			head = head.next;
+			return true;
+		}
+		
+		LinkedListItem item = head;
+		int i = 0;
+		while (i < index-1) {
+			i++;
+			item = item.next;
+			if (item == null) {
+				// index is out of linkedlist's range
+				return false;
+			}
+		}
+		
+		item.next = item.next.next;
+		return true;
+	}
+}
---
+++ src/dsa4/Main.java
@@ -0,0 +1,39 @@
+package dsa4;
+
+public class Main {
+	public static void main(String[] args) {
+		// Init linkedlist
+		BE8LinkedList ll = new BE8LinkedList();
+		ll.head = new LinkedListItem(0);
+		ll.head.next = new LinkedListItem(1);
+		ll.head.next.next = new LinkedListItem(2);
+		printLinkedList(ll);
+		
+		// Insert new item at any index
+		System.out.println("----- INSERT -----");
+		ll.insert(3, 33);
+		printLinkedList(ll);
+		ll.insert(2, 22);
+		printLinkedList(ll);
+		ll.insert(0, 11);
+		printLinkedList(ll);
+		
+		// Delete new item at any index
+		System.out.println("----- DELETE -----");
+		ll.delete(0);
+		printLinkedList(ll);
+		ll.delete(2);
+		printLinkedList(ll);
+		ll.delete(3);
+		printLinkedList(ll);
+	}
+	
+	public static void printLinkedList(BE8LinkedList ll) {
+		LinkedListItem item = ll.head;
+		while (item != null) {
+			System.out.print(item.value + " ");
+			item = item.next;
+		}
+		System.out.println();
+	}
+}
