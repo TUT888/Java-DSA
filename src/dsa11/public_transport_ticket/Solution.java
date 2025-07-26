70c1cd8f61e2559a7fad81663a30b92ad7590ae7
Tat Uyen Tam <tattam192@gmail.com>
2025-07-26 16:27:28 +1000

    fix(dsa11): update correct solution for ticket problem and more test cases for six digits

--- src/dsa11/public_transport_ticket/Solution.java
+++ src/dsa11/public_transport_ticket/Solution.java
@@ -1,7 +1,41 @@
 package dsa11.public_transport_ticket;
 
 public class Solution {
-	public static int solution(int[] A) {
+	public static int minCost(int[] A) {
+		return dynamicSolution(0, A, new int[A.length]);
+	}
+	
+	public static int dynamicSolution(int dateIndex, int[] A, int[] memo) {
+		if (dateIndex == A.length) return 0;
+		if (memo[dateIndex] != 0) return memo[dateIndex];
+		
+		int currentDay = A[dateIndex];
+		
+		// Opt 1: Use 1-day ticket
+		int costFor1DayTicket = 2 + dynamicSolution(dateIndex+1, A, memo);
+		
+		// Opt 2: Use 7-day ticket
+		int next7dayIndex = dateIndex + 1;
+		while (next7dayIndex < A.length && A[next7dayIndex] <= currentDay + 6) {
+			next7dayIndex += 1;
+		}
+		int costFor7dayTicket = 7 + dynamicSolution(next7dayIndex, A, memo);
+		
+		// Opt 3: Use 30-day ticket
+		int next30dayIndex = dateIndex + 1;
+		while (next30dayIndex < A.length && A[next30dayIndex] <= currentDay + 29) {
+			next30dayIndex += 1;
+		}
+		int costFor30dayTicket = 25 + dynamicSolution(next30dayIndex, A, memo);
+		
+		// Find minimum cost
+		int currentMinCost = Math.min(Math.min(costFor1DayTicket, costFor7dayTicket), costFor30dayTicket);
+		memo[dateIndex] = currentMinCost;
+		return currentMinCost;
+	}
+	
+	// !!! Greedy but not always right
+	public static int greedySolution(int[] A) {
 		int minimumCost = 0;
 		
 		int start = 0;
--- src/dsa11/six_digits/Main.java
+++ src/dsa11/six_digits/Main.java
@@ -2,16 +2,29 @@ package dsa11.six_digits;
 
 public class Main {
 	public static void main(String[] args) {
-		int[] arr = new int[] {1, 8, 3, 2, 6, 4};
-		System.out.println(Solution.solution(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]));
-
-		arr = new int[] {0, 0, 0, 0, 0, 0};
-		System.out.println(Solution.solution(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]));
-
-		arr = new int[] {0, 0, 0, 7, 8, 9};
-		System.out.println(Solution.solution(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]));
+		System.out.println(Solution.solution(1, 8, 3, 2, 6, 4)); // 12:36:48
+		System.out.println(Solution.solution(0, 0, 0, 0, 0, 0)); // 00:00:00
+		System.out.println(Solution.solution(0, 0, 0, 7, 8, 9)); // 07:08:09
+		System.out.println(Solution.solution(2, 4, 5, 9, 5, 9)); // NOT POSSIBLE
+		System.out.println(Solution.solution(2, 4, 6, 1, 5, 7)); // 12:46:57
 		
-		arr = new int[] {2, 4, 5, 9, 5, 9};
-		System.out.println(Solution.solution(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]));
+		System.out.println(Solution.solution(1, 3, 5, 8, 8, 9)); // 18:38:59
+		System.out.println(Solution.solution(2, 3, 4, 5, 6, 6)); // 23:46:56
+		System.out.println(Solution.solution(2, 3, 5, 5, 9, 9)); // 23:59:59
+		System.out.println(Solution.solution(1, 3, 4, 7, 8, 9)); // 17:38:49
+		
+		System.out.println(Solution.solution(2, 3, 1, 0, 5, 6)); // 01:23:56
+		System.out.println(Solution.solution(1, 2, 4, 5, 6, 9)); // 12:46:59
+		System.out.println(Solution.solution(2, 4, 5, 9, 5, 9)); // NOT POSSIBLE
+		
+		System.out.println(Solution.solution(1, 2, 3, 4, 5, 6)); // 12:34:56
+		System.out.println(Solution.solution(2, 4, 5, 9, 5, 9)); // NOT POSSIBLE
+		System.out.println(Solution.solution(0, 1, 2, 3, 4, 5)); // 01:23:45
+		System.out.println(Solution.solution(9, 9, 9, 9, 9, 9)); // NOT POSSIBLE
+		System.out.println(Solution.solution(2, 3, 5, 6, 7, 8)); // NOT POSSIBLE
+		System.out.println(Solution.solution(2, 3, 5, 5, 5, 5)); // 23:55:55
+		System.out.println(Solution.solution(0, 1, 0, 2, 0, 3)); // 00:01:23
+		System.out.println(Solution.solution(1, 1, 2, 2, 5, 9)); // 11:22:59
+		System.out.println(Solution.solution(0, 6, 7, 8, 9, 9)); // NOT POSSIBLE
 	}
 }
